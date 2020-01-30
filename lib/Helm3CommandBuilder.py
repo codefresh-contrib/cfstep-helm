2264ca56758b69881069c29f263c47ba1d03ad33
pashavictorovich <pavel@codefresh.io>
2020-01-30 16:09:19 +0200

    build helm command without force

--- lib/Helm3CommandBuilder.py
+++ lib/Helm3CommandBuilder.py
@@ -7,3 +7,6 @@ class Helm3CommandBuilder(BaseCommandBuilder):
         lines = super().build_export_commands(google_application_credentials_json)
         lines.append('export HELM_PLUGINS=/root/.helm/plugins')
         return lines
+
+    def build_helm_upgrade_command(self, release_name, chart_ref):
+        return 'helm upgrade %s %s --install --reset-values ' % (release_name, chart_ref)
