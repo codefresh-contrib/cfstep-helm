89eb1b6b936ddd09dcb91b07d0f66d4bcac9f1cf
pashavictorovich <pavel@codefresh.io>
2020-02-11 16:24:25 +0200

    repo yaml not found

--- lib/Helm3CommandBuilder.py
+++ lib/Helm3CommandBuilder.py
@@ -5,7 +5,6 @@ class Helm3CommandBuilder(BaseCommandBuilder):
 
     def build_export_commands(self, google_application_credentials_json):
         lines = super().build_export_commands(google_application_credentials_json)
-        lines.append('export HELM_PLUGINS=/root/.helm/plugins')
         return lines
 
     def build_helm_upgrade_command(self, release_name, chart_ref):
