63ca3765a60c125b7ab2a8722c48d398de605091
pashavictorovich <pavel@codefresh.io>
2020-02-11 17:28:58 +0200

    repo yaml not found

--- lib/Helm3CommandBuilder.py
+++ lib/Helm3CommandBuilder.py
@@ -5,6 +5,9 @@ class Helm3CommandBuilder(BaseCommandBuilder):
 
     def build_export_commands(self, google_application_credentials_json):
         lines = super().build_export_commands(google_application_credentials_json)
+        lines.append('export XDG_CACHE_HOME=/root/.helm')
+        lines.append('export XDG_DATA_HOME=/root/.helm')
+        lines.append('export XDG_CONFIG_HOME=/root/.helm')
         return lines
 
     def build_helm_upgrade_command(self, release_name, chart_ref):
