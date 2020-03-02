d16dd072dff54dfdcb299fca8d81c2c9869eaa0a
andrii.shaforostov <andrii@codefresh.io>
2020-03-02 19:50:45 +0200

    test

--- lib/EntrypointScriptBuilder.py
+++ lib/EntrypointScriptBuilder.py
@@ -161,7 +161,7 @@ class EntrypointScriptBuilder(object):
     def _build_version_commands(self):
         lines = []
 
-        kubectl_version_cmd = 'kubectl version'
+        kubectl_version_cmd = 'echo version'
         if self.dry_run:
             kubectl_version_cmd = 'echo ' + kubectl_version_cmd
         lines.append(kubectl_version_cmd)
