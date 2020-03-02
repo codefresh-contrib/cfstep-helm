15e8370dab61eab989998e22c7ce4f13070ca3ae
andrii.shaforostov <andrii@codefresh.io>
2020-03-02 19:25:30 +0200

    test

--- lib/EntrypointScriptBuilder.py
+++ lib/EntrypointScriptBuilder.py
@@ -160,13 +160,12 @@ class EntrypointScriptBuilder(object):
 
     def _build_version_commands(self):
         lines = []
-        if self.action in ['install', 'promotion']:
-            if self.kube_context is None:
-                raise Exception('Must set KUBE_CONTEXT in environment (Name of Kubernetes cluster as named in Codefresh)')
-            kubectl_cmd = 'kubectl config use-context "%s"' % self.kube_context
-            if self.dry_run:
-                kubectl_cmd = 'echo ' + kubectl_cmd
-            lines.append(kubectl_cmd)
+        if self.kube_context is None:
+            raise Exception('Must set KUBE_CONTEXT in environment (Name of Kubernetes cluster as named in Codefresh)')
+        kubectl_cmd = 'kubectl config use-context "%s"' % self.kube_context
+        if self.dry_run:
+            kubectl_cmd = 'echo ' + kubectl_cmd
+        lines.append(kubectl_cmd)
         return lines
 
     def _build_helm_commands(self):
