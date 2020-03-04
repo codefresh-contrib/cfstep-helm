8e8f52595cef8db18c785f69c54c20d99372b9fc
andrii.shaforostov <andrii@codefresh.io>
2020-03-04 14:04:10 +0200

    `use-context` for `auth` action

--- lib/EntrypointScriptBuilder.py
+++ lib/EntrypointScriptBuilder.py
@@ -149,7 +149,7 @@ class EntrypointScriptBuilder(object):
 
     def _build_kubectl_commands(self):
         lines = []
-        if self.action in ['install', 'promotion']:
+        if self.action in ['install', 'promotion', 'auth']:
             if self.kube_context is None:
                 raise Exception('Must set KUBE_CONTEXT in environment (Name of Kubernetes cluster as named in Codefresh)')
             kubectl_cmd = 'kubectl config use-context "%s"' % self.kube_context
