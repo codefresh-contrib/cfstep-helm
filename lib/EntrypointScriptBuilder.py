886b175fe75d82932ed54f0af16aaf5d5945fb94
nazar-codefresh <nazar@codefresh.io>
2020-03-13 15:13:58 +0200

    add correct resolve dependency for push

--- lib/EntrypointScriptBuilder.py
+++ lib/EntrypointScriptBuilder.py
@@ -284,11 +284,10 @@ class EntrypointScriptBuilder(object):
             helm_repo_add_cmd = 'echo ' + helm_repo_add_cmd
         lines.append(helm_repo_add_cmd)
 
-        if not self._helm_3():
-            helm_dep_build_cmd = 'helm dependency build %s' % self.chart_ref
-            if self.dry_run:
-                helm_dep_build_cmd = 'echo ' + helm_dep_build_cmd
-            lines.append(helm_dep_build_cmd)
+        helm_dep_build_cmd = 'helm dependency update %s' % self.chart_ref
+        if not self._helm_3() and self.dry_run:
+            helm_dep_build_cmd = 'echo ' + helm_dep_build_cmd
+        lines.append(helm_dep_build_cmd)
 
         if self.dry_run:
             package_var = 'dryrun-0.0.1.tgz'
