39b277ebd60b5e0b41dd54b9578c1dc36a47a58d
nazar-codefresh <nazar@codefresh.io>
2020-03-13 16:04:12 +0200

    improve update command

--- lib/EntrypointScriptBuilder.py
+++ lib/EntrypointScriptBuilder.py
@@ -284,7 +284,9 @@ class EntrypointScriptBuilder(object):
             helm_repo_add_cmd = 'echo ' + helm_repo_add_cmd
         lines.append(helm_repo_add_cmd)
 
-        helm_dep_build_cmd = 'helm dependency build %s' % self.chart_ref
+        helm_dep_build_cmd = 'helm dependency build {} || ' \
+                             'helm dependency update {} || ' \
+                             'echo "dependencies cannot be updated"'.format(self.chart_ref, self.chart_ref)
         if not self._helm_3() and self.dry_run:
             helm_dep_build_cmd = 'echo ' + helm_dep_build_cmd
         lines.append(helm_dep_build_cmd)
