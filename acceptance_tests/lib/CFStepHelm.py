a7a991b7a908f09085f93754f6455dee6a9f59f3
nazar-codefresh <nazar@codefresh.io>
2020-03-30 16:09:06 +0300

    delete unrelated changes

--- acceptance_tests/lib/CFStepHelm.py
+++ acceptance_tests/lib/CFStepHelm.py
@@ -1,9 +1,9 @@
-from acceptance_tests.lib.common import CommandRunner
+import common
 
 
-class CFStepHelm(CommandRunner):
+class CFStepHelm(common.CommandRunner):
     def run_with_env(self, env={}):
-        self.run_command_with_env(['/home/kodex/work/cfstep-helm/bin/release_chart'], False, env)
+        self.run_command_with_env(['release_chart'], False, env)
 
     def source_with_env_and_check_for_var(self, env={}, var=''):
         self.run_command_with_env([
