de630643807b57381860b77f2c98812f8c6d689a
nazar-codefresh <nazar@codefresh.io>
2020-03-30 12:18:16 +0300

    try fix

--- lib/EntrypointScriptBuilder.py
+++ lib/EntrypointScriptBuilder.py
@@ -250,17 +250,17 @@ class EntrypointScriptBuilder(object):
             lines.append(helm_dep_build_cmd)
 
         chart_path = "{}/{}".format(DOWNLOAD_CHART_DIR, self.chart_ref.split("/")[-1])
- #       helm_pull_cmd = self.helm_command_builder.build_pull_command() + ' {} --untar --untardir {} '.format(self.chart_ref, DOWNLOAD_CHART_DIR)
+        helm_pull_cmd = self.helm_command_builder.build_pull_command() + ' {} --untar --untardir {} '.format(self.chart_ref, DOWNLOAD_CHART_DIR)
 
-#        if self.chart_repo_url is not None:
-#            helm_pull_cmd += '--repo %s ' % self.chart_repo_url
-#
-#        if self.chart_version is not None:
-#            helm_pull_cmd += '--version %s ' % self.chart_version
- #       lines.append(helm_pull_cmd)
+        if self.chart_repo_url is not None:
+            helm_pull_cmd += '--repo %s ' % self.chart_repo_url
 
-#        if self.commit_message is not None:
-#            lines.extend(CommitMessageResolver.get_command(chart_path + '/templates/NOTES.txt', self.commit_message))
+        if self.chart_version is not None:
+            helm_pull_cmd += '--version %s ' % self.chart_version
+        lines.append(helm_pull_cmd)
+
+        if self.commit_message is not None:
+            lines.extend(CommitMessageResolver.get_command(chart_path + '/templates/NOTES.txt', self.commit_message))
 
         helm_upgrade_cmd = self.helm_command_builder.build_helm_upgrade_command(self.release_name, chart_path)
 
