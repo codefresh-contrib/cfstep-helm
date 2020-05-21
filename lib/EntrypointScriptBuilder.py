b4e77e7de993569129b1215d71f8f384a22d5ca8
nazar-codefresh <51325632+nazar-codefresh@users.noreply.github.com>
2020-05-21 13:51:42 +0300

    add auth kube context ability (#51)

--- lib/EntrypointScriptBuilder.py
+++ lib/EntrypointScriptBuilder.py
@@ -171,14 +171,17 @@ class EntrypointScriptBuilder(object):
 
     def _build_kubectl_commands(self):
         lines = []
-        if self.action in ['install', 'promotion']:
-            if self.kube_context is None:
+        if self.action in ['install', 'promotion', 'auth']:
+            if self.kube_context is not None:
+                kubectl_cmd = 'kubectl config use-context "%s"' % self.kube_context
+                if self.dry_run:
+                    kubectl_cmd = 'echo ' + kubectl_cmd
+                lines.append(kubectl_cmd)
+
+            if self.kube_context is None and self.action is not 'auth':
                 raise Exception(
                     'Must set KUBE_CONTEXT in environment (Name of Kubernetes cluster as named in Codefresh)')
-            kubectl_cmd = 'kubectl config use-context "%s"' % self.kube_context
-            if self.dry_run:
-                kubectl_cmd = 'echo ' + kubectl_cmd
-            lines.append(kubectl_cmd)
+
         return lines
 
     @staticmethod
