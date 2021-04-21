cbf0aa00c274c0ac58b9bf0fd46c72ab5d7fdb95
Vadim Kharin <vadim.kharin@codefresh.io>
2021-04-21 16:36:44 +0300

    CR-4119 add test

--- tests/EntrypointScriptBuilderTest.py
+++ tests/EntrypointScriptBuilderTest.py
@@ -1,4 +1,8 @@
 import unittest
+import os
+import sys
+parent_dir_name = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+sys.path.append(parent_dir_name)
 from lib.EntrypointScriptBuilder import EntrypointScriptBuilder
 
 
